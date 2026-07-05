0cdb1aa24de588a568aec5adc5f16d0949c63b2c
dttric <87319664+dttric@users.noreply.github.com>
2026-07-05 19:15:34 +0700

    ьояит крммиеиый срач

--- widget.py
+++ widget.py
@@ -8,8 +8,8 @@ min, max = api.get_progress(config["sid"], config["gid"]).split(":")
 
 discord = {
     "token": os.getenv("DISCORD_TOKEN"),
-    "appid": int(os.getenv("DISCORD_APP_ID")).strip(),
-    "uid": int(os.getenv("DISCORD_USER_ID")).strip()
+    "appid": int(os.getenv("DISCORD_APP_ID")),
+    "uid": int(os.getenv("DISCORD_USER_ID"))
 }
 
 objective = f"{int(min)/int(max)*100:.0f}%"
