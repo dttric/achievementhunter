575820b84c80f0c9a7bfe473503d278dcfaa67f7
dttric <87319664+dttric@users.noreply.github.com>
2026-07-06 20:04:22 +0700

    раз сделал то еще хуйню накину которая смущала

--- override.py
+++ override.py
@@ -1,1 +1,4 @@
-override_name = {632470: "Disco Elysium"}
+override_name = {
+    632470: "Disco Elysium",  # почему-то называется в API "No Truce With Furries"
+    250900: "The Binding of Issac: Rebirth",  # нету ':' в названии из API
+}
