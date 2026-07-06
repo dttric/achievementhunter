1958c5499b1467a77084ce101b0893990dc31033
dttric <87319664+dttric@users.noreply.github.com>
2026-07-06 19:50:13 +0700

    да да теперь у вас 0 ачивок

--- api.py
+++ api.py
@@ -1,38 +1,35 @@
-import requests
 import os
+
+import requests
 
 # Этот файл взаимодействует с Steam Web API для получения инфы о игре и игроке
 
 api_key = os.getenv("STEAM_API_KEY")
 
+
 def get_progress(sid, gid):
     url = "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/"
-    params = {
-        "key": api_key,
-        "steamid": sid,
-        "appid": gid
-    }
+    params = {"key": api_key, "steamid": sid, "appid": gid}
     response1 = requests.get(url, params=params)
 
     url2 = "http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/"
-    params2 = {
-        "key": api_key,
-        "appid": gid
-    }
+    params2 = {"key": api_key, "appid": gid}
 
     try:
         response2 = requests.get(url2, params=params2)
     except:
         return "Error fetching schema"
+    try:
+        return f"{sum(1 for achievement in response1.json()['playerstats']['achievements'] if achievement['achieved'] == 1)}:{len(response2.json()['game']['availableGameStats']['achievements'])}"
+    except KeyError:
+        return (
+            f"0:{len(response2.json()['game']['availableGameStats']['achievements'])}"
+        )
 
-    return f"{sum(1 for achievement in response1.json()["playerstats"]["achievements"] if achievement["achieved"] == 1)}:{len(response2.json()["game"]["availableGameStats"]["achievements"])}"
 
 def get_game(gid):
     url = "http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/"
-    params = {
-        "key": api_key,
-        "appid": gid
-    }
+    params = {"key": api_key, "appid": gid}
     response = requests.get(url, params=params)
     return response.json()["game"]["gameName"]
 
@@ -40,17 +37,19 @@ def get_game(gid):
 def get_image(gid):
     return {
         "hero": f"https://steamcdn-a.akamaihd.net/steam/apps/{gid}/hero_capsule.jpg",
-        "icon": f"https://steamcdn-a.akamaihd.net/steam/apps/{gid}/community_icon.jpg" # <---- не работает бтв, вырежу/заменю позже
+        "icon": f"https://steamcdn-a.akamaihd.net/steam/apps/{gid}/community_icon.jpg",  # <---- не работает бтв, вырежу/заменю позже
     }
+
 
 def get_player(sid):
     url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/"
-    params = {
-        "key": api_key,
-        "steamids": sid
-    }
+    params = {"key": api_key, "steamids": sid}
     response = requests.get(url, params=params)
-    return { "name": response.json()["response"]["players"][0]["personaname"], "avatar": response.json()["response"]["players"][0]["avatarfull"] }
+    return {
+        "name": response.json()["response"]["players"][0]["personaname"],
+        "avatar": response.json()["response"]["players"][0]["avatarfull"],
+    }
+
 
 if __name__ == "__main__":
     print(get_game(250900))
